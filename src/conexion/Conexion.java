4a4a93b0cc9891095ee204a8b6ef8d4589b7dbff
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2024-08-14 21:34:40 -0300

    Cambio de clase conexion de worbench a  xampp

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -10,17 +10,13 @@ import java.sql.SQLException;
  */
 public class Conexion {
 
-    //conexion local
     public static Connection conectar() {
-        try {
-
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_sistema_ventas?useSSL=false", "root", "jorge007");
-
-            return cn;
-        } catch (SQLException e) {
-            System.out.println("Error en la conexion local" + e);
-        }
-        return null;
-
+    try {
+        Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/systemadeventaenjava?useSSL=false", "root", "");
+        return cn;
+    } catch (SQLException e) {
+        System.out.println("Error en la conexion local: " + e);
     }
+    return null;
+}
 }
